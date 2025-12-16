a6edc1d17cfed15b192a64cbc6ab23fb09664b3e
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2025-12-16 12:42:12 +0530

    finished UI of Navbar component using prebuiltUi

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,13 @@
 import React from "react";
+import Navbar from "./components/Navbar.jsx";
 
 const App = () => {
   return (
     <div>
+      <Navbar />
       <h1 class="text-3xl font-bold underline">Hello world!</h1>
     </div>
   );
 };
 
 export default App;
- 
