2c44cd5a25865a753849a539c51ff4817610bf3b
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2025-12-16 19:09:50 +0530

    added routes for app and create some condition for sellerPath

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 import Navbar from "./components/Navbar.jsx";
+import Home from "./pages/Home.jsx";
+import { Route, Routes, useLocation } from "react-router-dom";
 
 const App = () => {
+  const isSellerPath = useLocation().pathname.includes("seller");
   return (
     <div>
-      <Navbar />
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+      {isSellerPath ? null : <Navbar />}
+      <div
+        className={`${isSellerPath ? "" : "px-6 md:px-16 lg:px-24 xl:px-32"}`}
+      >
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+
+      <h1 class="text-3xl font-bold underline"></h1>
     </div>
   );
 };
