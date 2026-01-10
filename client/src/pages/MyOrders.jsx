c41ee7ffb90ad81eefbc5d6fa7eae8611c3404e5
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2026-01-10 16:33:42 +0530

    feat: implement MyOrders page with order fetching and styling

--- client/src/pages/MyOrders.jsx
+++ client/src/pages/MyOrders.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useAppContext } from "../context/AppContext";
+import { dummyOrders } from "../assets/assets";
 
 const MyOrders = () => {
+  const [myOrders, setMyOrders] = useState([]);
+  const { currency } = useAppContext();
+
+  const fetchMyOrders = async () => {
+    setMyOrders(dummyOrders);
+  };
+
+  useEffect(() => {
+    fetchMyOrders();
+  }, []);
   return (
-    <div>
-        <p>My orders</p>
+    <div className="mt-16 pb-16">
+      <div className="flex flex-col items-end w-max mb-8">
+        <p className="text-2xl font-medium uppercase">My orders</p>
+        <div className="w-16 h-0.5 bg-primary rounded-full"></div>
+      </div>
     </div>
-  )
-}
- 
-export default MyOrders
+  );
+};
+
+export default MyOrders;
