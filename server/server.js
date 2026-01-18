73bdffadd0d50ec35c88a905d073d5958b3ea1a1
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2026-01-18 19:54:49 +0530

    created route for API endpoint register

--- server/server.js
+++ server/server.js
@@ -2,7 +2,8 @@ import cookieParser from "cookie-parser";
 import express from "express";
 import cors from "cors";
 import connectDB from "./configs/db.js";
-import 'dotenv/config'
+import "dotenv/config";
+import userRouter from "./routes/userRoute.js";
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -23,6 +24,7 @@ app.use(
 );
 
 app.get("/", (req, res) => res.send("API is Working"));
+app.use("/api/user", userRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
