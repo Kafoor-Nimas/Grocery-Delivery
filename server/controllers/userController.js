bd1d8366587223ba4dbd14a54a8c3913bce6ffda
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2026-01-18 18:34:09 +0530

    created controller function to regiter any user

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,6 @@
 import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 //Register :/api/user/register
 export const register = async (req, res) => {
@@ -13,5 +15,28 @@ export const register = async (req, res) => {
 
     if (existingUser)
       return res.json({ success: false, message: "User already exists" });
-  } catch (error) {}
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({ name, email, password: hashedPassword });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    res.cookie("token", token, {
+      httpOnly: true, //Prevent JavaScript to access cookie
+      secure: process.env.NODE_ENV === "production", //Use secure cookies in production
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict", //CSRF protection
+      maxAge: 7 * 24 * 60 * 60 * 1000, //cookie expiration time 7d
+    });
+
+    return res.json({
+      success: true,
+      user: { email: user.email, name: user.name },
+    });
+  } catch (error) {
+    console.log(error.message)
+    res.json({success:false,message:error.message})
+  }
 };
