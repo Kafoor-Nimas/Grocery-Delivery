77211f72b978d2e3f1fb03075434aaf1e10135a9
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2026-01-25 17:26:37 +0530

    created api function for seller login

--- server/controllers/sellerController.js
+++ server/controllers/sellerController.js
@@ -1,3 +1,37 @@
-
+import jwt from "jsonwebtoken";
 
 //Login Seller : /api/seller/login
+export const sellerLogin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (
+      email === process.env.SELLER_EMAIL &&
+      password === process.env.SELLER_PASSWORD
+    ) {
+      const token = jwt.sign({ email }, process.env.JWT_SECRET, {
+        expiresIn: "7d",
+      });
+
+      res.cookie("sellerToken", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+      });
+
+      return res.json({
+        success: true,
+        message: "Seller logged in successfully",
+      });
+    } else {
+      return res.json({
+        success: false,
+        message: "Invalid Seller Credentials",
+      });
+    }
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
