df7263eab5fe94cfd5936f2116100f455b02e17c
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2026-01-26 19:16:01 +0530

    created controller function to update inStock

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -49,4 +49,13 @@ export const productById = async (req, res) => {
 };
 
 //Change Product inStock : /api/product/stock
-export const changeStock = async (req, res) => {};
+export const changeStock = async (req, res) => {
+  try {
+    const { id, inStock } = req.body;
+    await Product.findByIdAndUpdate(id, { inStock });
+    res.json({ success: true, message: "Stock Updated" });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
