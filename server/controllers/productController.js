40f35cb0a0a3266cccabc88fa3f61afe6e8a94dd
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2026-01-26 17:50:42 +0530

    Implement addProduct function with image upload to Cloudinary

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,5 +1,22 @@
+import { v2 as cloudinary } from "cloudinary";
+
 //Add Product : /api/product/add
-export const addProduct = async (req, res) => {};
+export const addProduct = async (req, res) => {
+  try {
+    let productData = JSON.parse(req.body.productData);
+
+    const images = req.files;
+
+    let imagesUrl = await Promise.all(
+      images.map(async (item) => {
+        let result = await cloudinary.uploader.upload(item.path, {
+          resource_type: "image",
+        });
+        return result.secure_url;
+      }),
+    );
+  } catch (error) {}
+};
 
 //Get Product : /api/product/list
 export const productList = async (req, res) => {};
