9f5a8aa402d3abd1403ead453301cfffd1bfa792
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2026-01-27 20:26:18 +0530

    add Address model with schema definition

---
+++ server/models/Address.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+
+const addressSchema = mongoose.Schema({
+
+})
+
+const Address = mongoose.models.address || mongoose.model("address", addressSchema);
+
+export default Address;
