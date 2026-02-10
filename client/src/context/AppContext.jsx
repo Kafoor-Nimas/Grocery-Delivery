7cc7fbd8c23503c9865693f47e9539a5fd33b80c
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2026-02-10 10:28:49 +0530

    completed functionality of cart items

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -121,6 +121,24 @@ export const AppContextProvider = ({ children }) => {
     fetchSeller();
   }, []);
 
+  //update database cart Items
+  useEffect(() => {
+    const updateCart = async () => {
+      try {
+        const { data } = await axios.post("/api/cart/update", { cartItems });
+
+        if (!data.success) {
+          toast.error(data.message);
+        }
+      } catch (error) {
+        toast.error(error.message);
+      }
+    };
+    if (user) {
+      updateCart();
+    }
+  }, [cartItems]);
+
   const value = {
     navigate,
     user,
